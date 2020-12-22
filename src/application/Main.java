920683029891b0fbfc3b49a2b5f237b1afc51ff7
Caroline S Fonseca <carolinefonseca@gec.inatel.br>
2020-12-22 02:38:26 -0300

    First Class: Position

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
-
+		Position pos = new Position(3,5);
+		System.out.println(pos);
 	}
 
 }
