bbb1b2ca469d2ed65d71968fda0536ba8088d138
Caroline S Fonseca <carolinefonseca@gec.inatel.br>
2020-12-26 16:13:13 -0300

    Handling Exceptions

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -9,25 +11,32 @@ import chess.ChessPosition;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		
-		while(true) {
-			UI.printBoard(chessMatch.getPieces());
-			System.out.println();
-			System.out.println("Source: ");
-			ChessPosition source = UI.readChessPosition(sc);
-			
-			System.out.println();
-			System.out.println("Target: ");
-			ChessPosition target = UI.readChessPosition(sc);
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-			
+
+		while (true) {
+			try {
+				UI.printBoard(chessMatch.getPieces());
+				
+				System.out.println();
+				System.out.println("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+
+				System.out.println();
+				System.out.println("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			} catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			} catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
-		
-		
+
 	}
 
 }
--- src/application/UI.java
+++ src/application/UI.java
@@ -14,14 +14,13 @@ public class UI {
 			String s = sc.nextLine();
 			char column = s.charAt(0);
 			int row = Integer.parseInt(s.substring(1));
-			return new ChessPosition(column,row);
-		}
-		catch(RuntimeException e) {
+			return new ChessPosition(column, row);
+		} catch (RuntimeException e) {
 			throw new InputMismatchException("Error reading chess position.");
 		}
-	
+
 	}
-	
+
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
@@ -42,7 +41,7 @@ public class UI {
 			if (piece.getColor() == Color.WHITE) {
 				System.out.print(piece);
 			} else {
-				
+
 				System.out.print(piece);
 			}
 		}
