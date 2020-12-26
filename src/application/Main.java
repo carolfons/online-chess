d2f26b0443408c8fb419ea670d3b5e46659e3bb4
Caroline S Fonseca <carolinefonseca@gec.inatel.br>
2020-12-26 15:56:09 -0300

    Moving Pieces

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,13 +1,32 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		
+		while(true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.println("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.println("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			
+		}
+		
 		
 	}
 
--- src/application/UI.java
+++ src/application/UI.java
@@ -1,27 +1,50 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessPiece;
+import chess.ChessPosition;
+import chess.Color;
 
 public class UI {
 
+	public static ChessPosition readChessPosition(Scanner sc) {
+		try {
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column,row);
+		}
+		catch(RuntimeException e) {
+			throw new InputMismatchException("Error reading chess position.");
+		}
+	
+	}
+	
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
-			System.out.print((8-i)+ " ");
+			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++) {
 				printPiece(pieces[i][j]);
 			}
-			
+
 			System.out.println();
 		}
-		
+
 		System.out.println("  a b c d e f g h");
 	}
+
 	private static void printPiece(ChessPiece piece) {
-		if (piece == null){
+		if (piece == null) {
 			System.out.print("-");
-		}
-		else {
-			System.out.print(piece);
+		} else {
+			if (piece.getColor() == Color.WHITE) {
+				System.out.print(piece);
+			} else {
+				
+				System.out.print(piece);
+			}
 		}
 		System.out.print(" ");
 	}
